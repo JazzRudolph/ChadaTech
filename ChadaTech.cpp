07607f4ca42b0524995c482757291da5bbe85629
Jazz Rudolph <jazzrudolph92@outlook.com>
2025-09-16 23:25:54 -0400

    Created the C++ file and set it up for the project.

---
+++ ChadaTech.cpp
@@ -0,0 +1,18 @@
+/*
+Project 1: Chada Tech
+9/16/2025
+Jazz Rudolph*\ 
+
+This project will simulate two clocks that will meet the international standard ISO 8601 and display the 12-hour and 24-hour time formats based on the users input.
+*/
+
+iostream
+#include <iomanip>
+#include <string>
+#include <double>
+
+using namespace std;
+int main() 
+{
+    
+}
