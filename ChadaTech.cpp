67f157f30da5c5eee71261005b73dfe10eb46f77
Jazz Rudolph <jazzrudolph92@outlook.com>
2025-09-19 02:45:59 -0400

    I was not writting oop code correct so I removed the clock functions and focused on creating while loops. The while loops are compiling correctly so now im going to move onto the next step.

--- ChadaTech.cpp
+++ ChadaTech.cpp
@@ -6,41 +6,43 @@ Jazz Rudolph*\
 This project will simulate two clocks that will meet the international standard ISO 8601 and display the 12-hour and 24-hour time formats based on the users input.
 */
 
-iostream
+#include <iostream>
 #include <iomanip>
 #include <string>
-#include <double>
-
 using namespace std;
 int main() 
 {
-    int hours, minutes, seconds; // This will store the time values
+    
     char colon; // This adds the : between the time values
     int clock12; // This will store the 12hr clock
     int clock24; // This will store the 24hr clock
-    int ampm; // This will store the AM or PM values
     int userMenu; // This will store the users menu choices
     int userInput; // This will store the users time input
 
-    int time(int hours, minutes, seconds); // This will store the users time input
-    int clock1() {
-        cout << "12-Hour Clock" << endl;
-        // This will display the 12-hour clock
 
+    int hour = 0; // This will store the hour value
+    int minute = 0; // This will store the minute value
+    int second = 0; // This will store the second value
+    int ampm; // This will store the AM or PM values
+
+
+
+
+    while (hour < 1 || hour > 12) { // This will store the users time input
+        cout << "Enter hour";
+        cin >> hour;
+    } 
+
+    while (minute < 0 || minute > 59) { // This will store the users time input
+        cout << "Enter minute";
+        cin >> minute;
+    }
+    
+    while (second < 0 || second > 59) { // This will store the users time input
+        cout << "Enter second";
+        cin >> second;
     }
 
-    int clock2() {
-        cout << "24-Hour Clock" << endl;
-        // This will display the 24-hour clock
-    }
-
-    int userMenu(int userInput) { // This will display the users menu choices
-        cout << "1-Add One Hour" << endl;
-        cout << "2-Add One Minute" << endl;
-        cout << "3-Add One Second" << endl;
-        cout << "4-Exit Program" << endl;
-        cin >> userMenu;
-        return userMenu;
-    }
+    return 0;
 
 }
